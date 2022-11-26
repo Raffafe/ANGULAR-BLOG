d42cf0c0e8adb4d78175a12a36375c0823a518d1
Raffaela Freitas <irsf84@gmail.com>
2022-11-26 16:33:47 +0700

    content created

---
+++ src/app/components/data/datafake.ts
@@ -0,0 +1,45 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Isekai wa Smartphone to Tomo ni revela trailer de sua segunda temporada",
+    "descripition":"No site oficial da adaptação para anime das light novels escritas por Patora Fuyuhara e ilustradas por Eiji Usatsuka , Isekai wa Smartphone to Tomo ni ( In Another World With My Smartphone ), foi divulgado um novo vídeo promocional da segunda temporada do projeto. O vídeo confirma que a estreia está marcada para abril de 2023 no Japão , quase seis anos após a estreia da primeira temporada.",
+    "photoCover" : "https://wallpaperaccess.com/full/3230464.png"
+
+  },
+  {
+    "id":"2",
+    "title": "Jujutsu Kaisen 0 supera Weathering With You em bilheteria mundial",
+    "descripition":"A equipe de produção do filme dos estúdios MAPPA , Jujutsu Kaisen 0 , relatou que o filme acumulou 20,51 milhões de vendas de ingressos para um faturamento mundial de 26,5 bilhões de ienes. No entanto, o portal The Numbers lista uma bilheteria mundial de mais de 195,87 milhões de dólares americanos. Ainda assim, ambos os números colocam Jujutsu Kaisen 0 como o sexto filme de anime de maior sucesso de todos os tempos , sem considerar a inflação.O filme superou Tenki no Ko de Makoto Shinkai , mas ainda está atrás de Kimetsu no Yaiba: Mugen Ressha-hen , três filmes de Hayao Miyazaki, bem como Makoto Shinkai filme de Kimi no Na wa.",
+    "photoCover" : "https://www.cnet.com/a/img/resize/aad12aa3367683e6d9846ec56e6b96d8d50f6acb/hub/2022/09/09/99594919-ebac-4f49-a9fd-f9f822ae9bc9/jujutsu-kaisen-movie.jpg?auto=webp&fit=crop&height=675&width=1200"
+  },
+  {
+    "id":"3",
+    "title": "Tsurune da Kyoto Animation data de estreia para sua segunda temporada",
+    "descripition":"No site oficial da adaptação para anime da light novel escrita por Kotoko Ayano e ilustrada por Chinatsu Morimoto , Tsurune: Kazemai Koukou Kyuudoubu ( Tsurune: Kazemai High School's Kyuudou Club ), um novo vídeo promocional para a segunda temporada do projeto foi publicado , que será intitulado Tsurune: Tsunagari no Issha ( Tsurune: The Linking Shot ). O vídeo confirma que a data de estreia está marcada para 4 de janeiro de 2023 no Japão ",
+    "photoCover": "http://d1a2e1vehwcxq9.cloudfront.net/news/2018/10/sentai-filmworks-tsurune-870x520.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Pole Princess!!, um anime centrado na dança do poste, anunciado",
+    "descripition":"Através de um comunicado de imprensa, os estúdios Tatsunoko Production anunciaram a produção de um novo anime original focado na temática de pole dance (pole dance), intitulado Pole Princess!! . A história é centrada em Hinano, uma garota de 17 anos muito normal e bem-educada. Desde muito pequena sua avó administrava um planetário que Hinano adorava, mas um dia Hinano descobriu que o planetário iria fechar. Hinano se preocupa se ela pode fazer algo por sua avó e pelo planetário, e um dia ela vê uma mulher dançando no poste sob um céu estrelado. Ela reúne as amigas e juntas montam um palco no planetário para fazer shows, e se lançam no mundo do pole dancing. O comunicado revelou o elenco de voz e a equipe de produção, mas não indicou uma data de estreia prevista.",
+    "photoCover": "https://i.ytimg.com/vi/Uz2et-JVd3c/maxresdefault.jpg"
+  },
+  {
+    "id":"5",
+    "title": "Love Live!! Nijigasaki terá um OVA no próximo verão",
+    "descripition":"Durante uma transmissão especial, foi anunciado que o Love Live! Nijigasaki Gakuen School Idol Doukoukai ( Love Live! Nijigasaki High School Idol Club ) terá um OVA que será lançado nos cinemas do Japão durante a temporada de verão-2023 (julho-setembro) . A declaração não é clara sobre o tipo de projeto, então é possível que seja um OVA, mas vários episódios.",
+    "photoCover": "https://img.quizur.com/f/img5d6acfb73511a7.61468083.jpg?lastEdited=1567281082"
+  },
+  {
+    "id":"6",
+    "title": "Spy x Family – Sucesso de audiência do anime continua surpreendendo canal de TV",
+    "descripition":"A obra manteve uma excelente média de audiência para o horário em que foi exibido (sábado de madrugada), ficando na casa dos 1,8%, mas o que mais impressionou foram os dados de time-shifting (programas gravados). Se somados, a audiência esperada para a obra seria de 5,6% (6% na estreia do dia 1º de outubro), o que fez com que a obra se tornasse a maior audiência em time-shifting dentre os canais de TV desde julho desse ano. Devido a esses números impressionantes, o presidente da TV Tokyo se questionou sobre ter sido uma decisão sábia dar essa faixa ruim de horário para o anime, e ressaltou que talvez seja a hora de fazer um estudo mais amplo nas grandes mudanças em como o público vem assistindo  programas de TV.",
+    "photoCover": "http://cabanadoleitor.com.br/wp-content/uploads/2022/07/spy-x-family-e-1280x720.webp"
+  },
+  {
+    "id":"7",
+    "title": "Shuumatsu no Valkyrie marca a estreia de sua segunda temporada com uma prévia",
+    "descripition":"No site oficial da adaptação para anime do mangá escrito por Takumi Fukui e Shinya Umemura , e ilustrado por Ajichika , Shuumatsu no Valkyrie ( Record of Ragnarok ), foi publicado um novo vídeo promocional da segunda temporada do projeto. O vídeo confirma que a série terá um total de quinze episódios , com a primeira parte (episódios 1 a 10) marcada para 26 de janeiro de 2023 globalmente na Netflix , e a segunda parte (episódios 11 a 15) marcada para algum dia. ano de 2023 .",
+    "photoCover": "https://i0.wp.com/www.jbox.com.br/wp/wp-content/uploads/2021/03/shuumatsu-anime-destacada.jpg?fit=774%2C489&quality=99&strip=all&ssl=1"
+  }
+];
